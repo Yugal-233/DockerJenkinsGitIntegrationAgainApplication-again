d746bc8fdbf8772ebafe380b4b9985c7e8e19895
yugal <yugalchaudhari233@gmail.com>
2023-06-30 13:10:24 +0530

    first commit

---
+++ src/main/java/com/devops/integration/DockerJenkinsGitIntegrationAgainApplication.java
@@ -0,0 +1,13 @@
+package com.devops.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsGitIntegrationAgainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsGitIntegrationAgainApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/devops/integration/controller/DockerGitJenkinsIntegration.java
@@ -0,0 +1,13 @@
+package com.devops.integration.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerGitJenkinsIntegration {
+
+	@GetMapping("/integration")
+	public String DockerGitJenkinsIntegrationData() {
+		return "DockerGitJenkinsIntegration is successfully Done"; 
+	}
+}
